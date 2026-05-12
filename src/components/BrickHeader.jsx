796bf4c59368b901279da2c8476e83b78aedf78d
Mustafa S <espnguy@gmail.com>
2026-05-12 12:38:00 -0500

    banner fixes

--- src/components/BrickHeader.jsx
+++ src/components/BrickHeader.jsx
@@ -3,28 +3,12 @@ export default function BrickHeader({ color, items, images }) {
     return (
       <div
         aria-hidden="true"
-        style={{
-          backgroundColor: color,
-          paddingTop: 12,
-          paddingBottom: 8,
-          paddingLeft: 8,
-          paddingRight: 8,
-        }}
+        className="w-full flex items-end justify-around px-3"
+        style={{ backgroundColor: color, height: 96 }}
       >
-        <div
-          className="w-full flex items-end justify-around"
-          style={{
-            backgroundColor: '#ffffff',
-            borderRadius: 10,
-            height: 88,
-            paddingLeft: 8,
-            paddingRight: 8,
-          }}
-        >
-          {images.map((src, i) => (
-            <img key={i} src={src} alt="" style={{ height: 82, width: 'auto' }} />
-          ))}
-        </div>
+        {images.map((src, i) => (
+          <img key={i} src={src} alt="" style={{ height: 84, width: 'auto' }} />
+        ))}
       </div>
     );
   }
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -7,7 +7,7 @@ export default function Welcome({ onYes, onNo, existingRsvp, onEdit }) {
     <div className="min-h-screen flex flex-col">
       <BrickHeader
         color="#F44336"
-        images={['/elmo.png', '/cookie-monster.png', '/abby.png', '/big-bird-transparent-6-3618470511.jpg', '/bert-ernie.png']}
+        images={['/elmo.png', '/cookie-monster.png', '/abby.png', '/bigbird.png', '/bert-ernie.png']}
       />
 
       <main className="flex-1 px-5 pt-6 pb-10 space-y-5">

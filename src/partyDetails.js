1a708fb75387067d2aea58ddc4284f166e5dabc3
Mustafa S <espnguy@gmail.com>
2026-05-21 17:47:11 -0500

    updated host and location

--- src/partyDetails.js
+++ src/partyDetails.js
@@ -4,11 +4,11 @@ export const PARTY = {
   title: "Rayyan's Sesame Street Party",
   subtitle: 'C is for Celebration!',
   dateLabel: 'Saturday, July 25 · 12:00 PM – 2:00 PM',
-  locationName: 'Cloudbound',
+  locationName: 'Cloudbound Frisco',
   locationAddress: '3333 Preston Rd. Ste 1200, Frisco, TX',
   locationMapUrl: 'https://www.google.com/maps/search/?api=1&query=3333+Preston+Rd+Ste+1200+Frisco+TX',
   calendarUrl: '/rayyan-birthday.ics',
-  hostName: 'Mustafa and Shazia',
+  hostName: 'Shazia and Mustafa',
   hostPhone: '214-435-1676',
   waiverUrl: 'https://waiver.haveablast.roller.app/CloudboundFriscoPrestonRidge/home',
 };
